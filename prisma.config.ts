d2c2868384e2feb4f968d2929c0b2f5bf2a45a4d
jigneshprajapati74 <jigneshprajapati745086@gmail.com>
2026-03-21 19:32:33 +0530

    update prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
   },
   datasources: {
     db: {
-      url: process.env.DATABASE_URL!,
+      url: process.env.DATABASE_URL || process.env.DIRECT_URL,
+      directUrl: process.env.DIRECT_URL || process.env.DATABASE_URL,
     },
   },
 });
